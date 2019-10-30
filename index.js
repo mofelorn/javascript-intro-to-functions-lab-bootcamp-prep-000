5533c2c586bf5f585b548d18e03e5b76fd229a98
Felix Moajdo Jr <mojadof@yahoo.com>
2019-10-30 15:30:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ function sayHiToGrandma(string){
 
 function sayHiToGrandma(string){
   if (string === "I love you, Grandma."){
+    return "I love you, too."
   } else if (string === string.toLowerCase()){
     return "I can't hear you!"
   } else {
